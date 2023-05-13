bd7d708f1d4839f7731812a645a36eaa3de79504
DiazDaw <victoriamorag99@gmail.com>
2023-05-13 23:11:43 +0200

    add new events to calendar and change color of it

--- AMS-front/src/app/pages/activities/activities.component.ts
+++ AMS-front/src/app/pages/activities/activities.component.ts
@@ -1,45 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import esLocale from '@fullcalendar/core/locales/es';
+import timeGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
   styleUrls: ['./activities.component.css']
 })
-export class ActivitiesComponent {
+export class ActivitiesComponent implements OnInit {
 
   description: string = 'Hola mundo';
+  arrayColors: string[] = ['#cdb4db', '#ffc8dd', '#ffafcc', '#bde0fe', '#a2d2ff'];
+  
 
+  arrayEvents: any[] = [
+    { title: 'Ofrenda', start: '2023-05-13', end: '2023-05-16' },
+    { title: 'event 2', date: '2023-05-14' }
+  ]
 
 
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin],
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin ],
     weekends: true,
-
-    events: [
-      { title: 'Ofrenda', date: '2023-05-13', color: "#ACE600", textColor: "#FF6600" },
-      { title: 'event 2', date: '2023-05-14' }
-    ],
-
+    events: this.arrayEvents.map((event, index) => {
+      return {
+        ...event,
+        color: this.arrayColors[index % this.arrayColors.length]
+      };
+    }),
+    eventColor: 'red',
+    eventTextColor: 'black',
+    locale: esLocale,
+    headerToolbar:{
+      left: 'prev,next today',
+      center: 'title'
+    }
   }
 
   constructor() {
-    console.log(this.calendarOptions.events);
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.calendarOptions);
 
   }
 
 
   toggleWeekends() {
-    this.calendarOptions.eventSources?.push({
-      events: [
-        { title: 'Nuevo evento', date: '2023-05-28', color: "#FF0000", textColor: "#FFFFFF" }
-      ]
-    });
-
-    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+    this.arrayEvents.push({ title: 'Nuevo evento', date: '2023-05-15' });
+    this.calendarOptions.events = this.arrayEvents.map((event, index) => {
+      return {
+        ...event,
+        color: this.arrayColors[index % this.arrayColors.length]
+      };
+    }),
+    this.calendarOptions = { ...this.calendarOptions };
+    console.log(this.arrayEvents);
   }
 
 }
